29bfc01f8fa327d9d6e6edac834506ccfe9bf444
Marco Martinez <marco.antoni.martinez@gmail.com>
2025-06-27 20:42:25 +0000

    Cleaned up test class

--- tests/tests_distance.rb
+++ tests/tests_distance.rb
@@ -6,7 +6,7 @@ class TestDistance < Minitest::Test
     dist_calc = Distance.new("City A", "City E")
     
     assert_equal "To go from City A to City E it will take you #{rand(50)} miles." , dist_calc.distance_calc("12"), "Distance calculator method failed"
-    #so condition testing, funtion we are testing, and error message for when it fails
+    
   end
 
 
